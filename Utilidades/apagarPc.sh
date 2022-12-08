1cc961f872f7c4403fbd45ad6a85d0bedb4f3b3d
ximenadmbs <ximenacbd@yahoo.com>
2022-12-08 13:35:25 -0600

    Se agregaron Plantilla y se agrego linea vertical apagarPc

--- Utilidades/apagarPc.sh
+++ Utilidades/apagarPc.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-clear
+    clear
+echo " "
+echo "+--------------------------------------------------+"
+echo "|      Ximena bash scrip [Versión 0.9.90]          |"
+echo "|  (c) Corporation. Todos los derechos reservados. |"
+echo "|                                                  |"
+echo "|    *** SCRIPT PARA EL APAGADO DEL EQUIPO ***     |"
+echo "+--------------------------------------------------+"
 echo " "
 echo " SELECCIONA UNA OPCIÓN:"
 echo " 1.-Apagar equipo ahora"
@@ -8,21 +15,22 @@ echo " 3.-Asignar hora de apagado del equipo"
 echo " 4.-Apagar equipo a los xx minutos"
 echo " 5.-Salir"
 echo " "
-read -p "OPCIÓN: " OPCION
-case $OPCION in
-1)sudo shutdown -h now;; 
-#sudo halt;;
-2) sudo reboot;;
-3) echo -n " ¿ A qué hora ?: "
-read hora
-sudo shutdown -h $hora;;
-4)echo -n " ¿ En cuántos minutos se apagará el equipo?: "
-read minutos
-sudo shutdown -h $minutos;;
-5) exit;;
-*) echo " OPCIÓN NO VÁLIDA "
- xit 1;;
-esac
-exit 0
+    read -p "OPCIÓN: " OPCION
+        case $OPCION in
+            1)sudo shutdown -h now;; 
+            #sudo halt;;
+            2) sudo reboot;;
+            3) echo -n " ¿ A qué hora ?: "
+    read hora
+        sudo shutdown -h $hora;;
+            4)echo -n " ¿ En cuántos minutos se apagará el equipo?: "
+        read minutos
+        sudo shutdown -h $minutos;;
+            5) exit;;
+            *) echo " OPCIÓN NO VÁLIDA "
+    exit 1;;
+        esac
+    exit 0
 
 # mie 09 nov 2022 14:45:34
+# jue 08 dic 2022 13:32:20 CST
