ab4eaf8ed4987881f180fba7d7e23fab821c2eea
ximenadmbs <ximenacbd@yahoo.com>
2022-12-08 13:13:25 -0600

    Se creo utilidades

---
+++ Utilidades/luzTeclado-off.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+xset led off
+
+echo "Se apagaron las luces del teclado"
+
+#chmod 777 luzTeclado-off.sh
+#Ejecutar con ./luzTeclado-off.sh
+
+#12:55 p. m. 03/09/2022
+
---
+++ Utilidades/luzTeclado.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+xset led 3
+
+echo "Se encendieron las luces del teclado"
+
+#chmod 777 luzTeclado.sh
+#Ejecutar con ./luzTeclado.sh
+# sudo apt install x11-xserver-utils
+
+#12:54 p. m. 03/09/2022
