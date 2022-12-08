b9c503f12509cd22a2edb8c486332fea046b9ecb
ximenadmbs <ximenacbd@yahoo.com>
2022-12-08 13:58:45 -0600

    Se modificarlon baner e instrucciones

--- Utilidades/mountRed-v1.7.sh
+++ Utilidades/mountRed-v1.7.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 echo " "
-echo -e "\e[0;38;5;141mXimena bash scrip [Versión 1.22.07]\e[0m"
+echo "+--------------------------------------------------+"
+echo "|      Ximena bash scrip [Versión 1.2]             |"
+echo "|  (c) Corporation. Todos los derechos reservados. |"
+echo "|                                                  |"
+echo "|   *** Se montara la unidadde red en la sig: ***  |"
+echo "|           direccion: ~/redMount                  |"
+echo "+--------------------------------------------------+"
+echo " "
     sudo mkdir ~/redMount
     sudo mount -t cifs -o username=xenon,password=14789,noperm //192.168.100.22/data-500gb ~/redMount
 echo "Se monto la carpeta de red en ~/redMount de este usuario"
