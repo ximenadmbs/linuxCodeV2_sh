00895f4e5378a015b742237cad5187a0b1c2e2f3
ximenadmbs <ximenacbd@yahoo.com>
2022-12-08 13:09:52 -0600

    Se creo edito linea vertical

--- Ubuntu22LTS/update-derivadasDEB.sh
+++ Ubuntu22LTS/update-derivadasDEB.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 echo " "
-echo "Ximena bash scrip [Versión 0.10]"
+echo "Ximena bash scrip [Versión 0.20]"
 echo "(c) Corporation. Todos los derechos reservados, "
 echo "██ Actualisador de paquetes y sistema, se iniciara las actualisaciones."
 echo ""
-sudo apt update
+    sudo apt update
 echo ""
 echo "██ Se Actialisaron los repositorios."
 echo "."
-sudo apt upgrade
+    sudo apt upgrade
 echo ""
 echo "██ Se actualisaron los paquetes seleccionados"
 echo "."
-sudo apt autoclean
+    sudo apt autoclean
 echo ""
 echo "██ ::::::::::Tareas terminadas::::::::::"
 echo " "
-date
+    date
 echo " "
 
 #chmod 777 update-v1.0.22.sh
 #Ejecutar con ./update-v1.22LTS.sh
 
 # lun 05 sep 2022 13:34:23 CDT
+# mié 09 sep 2022 12:52:45 CDT
