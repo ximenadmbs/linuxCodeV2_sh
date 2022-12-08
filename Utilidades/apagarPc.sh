01243e65093f281399eb69f67ba6bd5be267d4ad
ximenadmbs <ximenacbd@yahoo.com>
2022-12-08 14:41:24 -0600

    Se editaron lineas y baner de plex

--- Utilidades/apagarPc.sh
+++ Utilidades/apagarPc.sh
@@ -15,22 +15,22 @@ echo " 3.-Asignar hora de apagado del equipo"
 echo " 4.-Apagar equipo a los xx minutos"
 echo " 5.-Salir"
 echo " "
-    read -p "OPCIÓN: " OPCION
-        case $OPCION in
-            1)sudo shutdown -h now;; 
-            #sudo halt;;
-            2) sudo reboot;;
-            3) echo -n " ¿ A qué hora ?: "
-    read hora
-        sudo shutdown -h $hora;;
-            4)echo -n " ¿ En cuántos minutos se apagará el equipo?: "
-        read minutos
-        sudo shutdown -h $minutos;;
-            5) exit;;
-            *) echo " OPCIÓN NO VÁLIDA "
-    exit 1;;
-        esac
-    exit 0
+read -p "OPCIÓN: " OPCION
+    case $OPCION in
+        1)sudo shutdown -h now;; 
+        #sudo halt;;
+        2) sudo reboot;;
+        3) echo -n " ¿ A qué hora ?: "
+            read hora
+            sudo shutdown -h $hora;;
+        4)echo -n " ¿ En cuántos minutos se apagará el equipo?: "
+            read minutos
+            sudo shutdown -h $minutos;;
+        5) exit;;
+        *) echo " OPCIÓN NO VÁLIDA "
+exit 1;;
+    esac
+exit 0
 
 # mie 09 nov 2022 14:45:34
 # jue 08 dic 2022 13:32:20 CST
--- Utilidades/plexInstall.sh
+++ Utilidades/plexInstall.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
 
+    clear
 echo " "
-echo "Ximena bash scrip (plex) [Versión 1.0]"
+echo "                 llll  "
+echo "                 llll  "
+echo -e " pppp dppppb.    llll     >>eeee>  \e[0;38;5;178m'xxxx.\e[0m   .xxxx'  "
+echo -e " ppppppppppppp.  llll   eeeee''eeee. \e[0;38;5;178m'xxxx.\e[0m'xxxx'  "
+echo -e " pppp'     'ppp. llll  eeee'    'eee. \e[0;38;5;178m'xxxx.\e[0m'xx'  "
+echo -e " pppp       pppi llll  eeeeeeeeeeeeee  \e[0;38;5;178m'xxxx>\e[0m'  "
+echo -e " pppp       pppi llll  eeee           \e[0;38;5;178m.xxxx'\e[0m xx  "
+echo -e " pppppp,,,pppp'  lllll  eeee...eeee; \e[0;38;5;178m.xxxx'\e[0m.xxxx  "
+echo -e " pppp pppppp'     llll'  'eeeeeee;  \e[0;38;5;178m.xxxx'\e[0m  'xxxx'  "
+echo -e " pppp   '''        ''       ''''    \e[0;38;5;178m'''''\e[0m     '''  "
+echo -e " pppp  "
+echo -e " '''  "
+echo " "
+echo "Ximena bash scrip (plex) [Versión 1.7]"
 echo "(c) Corporation. Todos los derechos reservados, "
 echo "Instalacion del Servidor plex"
 
-sudo apt update
-sudo apt upgrade
+#se actualisara el sistema antes de la instalacion:
+    sudo apt update
+    sudo apt upgrade
 
-sudo apt install apt-transport-https curl wget -y
-sudo wget -O- https://downloads.plex.tv/plex-keys/PlexSign.key | gpg --dearmor | sudo tee /usr/share/keyrings/plex.gpg
-echo deb [signed-by=/usr/share/keyrings/plex.gpg] https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+#seccion de instalacion de herramintas nesesarias:
+    sudo apt install apt-transport-https curl wget -y
 
-sudo apt update
+#Se bajaran los ficheros nesesarios:
+    sudo wget -O- https://downloads.plex.tv/plex-keys/PlexSign.key | gpg --dearmor | sudo tee /usr/share/keyrings/plex.gpg
+    echo deb [signed-by=/usr/share/keyrings/plex.gpg] https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
 
-sudo apt install plexmediaserver -y
+#se actualisara el sistema para la detencion de ficheros y se instalara el paquete.
+    sudo apt update
+    sudo apt install plexmediaserver -y
 
-systemctl status plexmediaserver.service
+#Se muestra el estatus del servicio de plex:
+    systemctl status plexmediaserver.service
 
-echo "[ ██ :::::Se acompleto la tarea de instalacion:::::]]]]"
+echo "██ :::::Se acompleto la tarea de instalacion:::::"
 echo " "
+    date
 
 #chmod 777 update-v1.0.22.sh
 #Ejecutar con ./update-v1.0.22.sh
 
 # mar 01 nov 2022 13:01:05 CST
+# jue 08 dic 2022 14:16:12 CST
