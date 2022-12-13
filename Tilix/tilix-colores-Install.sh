787ff87093dfe3a0f25d77cc6dac0b7c5e1b1b2b
ximenadmbs <ximenacbd@yahoo.com>
2022-12-13 15:32:26 -0600

    Se agregaron al repositorio

---
+++ Tilix/tilix-colores-Install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo " "
+echo "+--------------------------------------------------+"
+echo "|      Ximena bash scrip [Versión 1.2]             |"
+echo "|  (c) Corporation. Todos los derechos reservados. |"
+echo "|                                                  |"
+echo "|      *** Tilix instalador de colores ***         |"
+echo "+--------------------------------------------------+"
+echo " "
+echo " configuracion-colores-tilix.tar.xz "
+	~/.config/
+echo " ~/.config/tilix/schemes/ "
+date
+echo " "
+
+#chmod 777 update-v1.0.22.sh
+#Ejecutar con ./update-v1.0.22.sh
