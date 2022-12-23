20cd6833e0098b76bf60237e43a6d3e441e42093
ximenadmbs <ximenacbd@yahoo.com>
2022-12-22 19:11:48 -0600

    Se actualiso el baner y color

--- Ubuntu22LTS/update-derivadasDEB.sh
+++ Ubuntu22LTS/update-derivadasDEB.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
+   *****************************************
+   * LoginControl 3.5 © 2004 José A. Gómez *
+   *      http://www.pandreonline.com      *
+   *****************************************
 echo " "
-echo "+--------------------------------------------------+"
-echo "|      Ximena bash scrip [Versión 1.2]             |"
-echo "|  (c) Corporation. Todos los derechos reservados. |"
-echo "|                                                  |"
-echo "|   *** SCRIPT ACTUALISADOR DERIVADAS DEBIAN ***   |"
-echo "+--------------------------------------------------+"
+echo "+-----------------------------------------------------+"
+echo "|          Ximena bash scrip [Versión 1.2]            |"
+echo "|           https://github.com/ximenadmbs             |"
+echo "|  © 2022 Corporation. Todos los derechos reservados. |"
+echo "|                                                     |"
+echo "|   *** SCRIPT ACTUALISADOR DERIVADAS DEBIAN ***      |"
+echo "+-----------------------------------------------------+"
 echo " "
-echo "██ Actualisador de paquetes y sistema, se iniciara las actualisaciones."
+echo -e "\e[0;38;5;208m█ Actualisador de paquetes y sistema, se iniciara las actualisaciones.\e[0m"
 echo ""
     sudo apt update
 echo ""
-echo "██ Se Actialisaron los repositorios."
+echo -e "\e[0;38;5;208m█ Se Actialisaron los repositorios.\e[0m"
 echo "."
     sudo apt upgrade -y
 echo ""
-echo "██ Se actualisaron los paquetes seleccionados"
+echo -e "\e[0;38;5;208m█ Se actualisaron los paquetes seleccionados\e[0m"
 echo "."
     sudo apt autoclean
     sudo apt autoremove
 echo ""
-echo "██ ::::::::::Tareas terminadas::::::::::"
+echo -e "\e[0;38;5;208m██ ::::::::::Tareas terminadas::::::::::\e[0m"
 echo " "
     date
 echo " "
@@ -32,3 +37,4 @@ echo " "
 # lun 05 sep 2022 13:34:23 CDT
 # mié 09 sep 2022 12:52:45 CDT
 # mar 01 nov 2022 13:01:05 CST
+# jue 22 dic 2022 19:08:50 CST
